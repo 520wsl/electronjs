efcd844ff3be714aaca09a63e885afbc0859695e
520wsl <395548460@qq.com>
2019-03-19 19:03:45 +0800

    feat(查排名列表):界面布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,214 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <User></User>
-  </div>
+    <!--<div class="home">-->
+    <!--<img src="../assets/logo.png">-->
+    <!--<User></User>-->
+    <!--<p>测试</p>-->
+    <!--</div>-->
+    <div>
+        <Card>
+            <div slot="title">喜小帮查排名</div>
+            <div slot="extra"><a>退出登录</a>微信昵称</div>
+            <section class="search">
+                <div class="search-options">店铺：
+                    <Select class="search-options-input">
+                        <Option :value="-1">乐青市柳市鞋厂</Option>
+                        <Option :value="-1">乐青市柳市鞋厂</Option>
+                        <Option :value="-1">乐青市柳市鞋厂</Option>
+                        <Option :value="-1">乐青市柳市鞋厂</Option>
+                        <Option :value="-1">乐青市柳市鞋厂</Option>
+
+                    </Select>
+                </div>
+                <div class="search-options">查询方式：
+                    <Select class="search-options-input">
+                        <Option :value="-1">销量</Option>
+                        <Option :value="1">综合</Option>
+
+                    </Select>
+                </div>
+                <div class="search-sub">
+                    <Button @click="search()" type="primary" icon="ios-search">查排名</Button>
+                </div>
+            </section>
+            <div class="content">
+                <Table :columns="columns1" :data="data1"></Table>
+                <div class="page">
+                    <!--<Page></Page>-->
+                </div>
+            </div>
+        </Card>
+        <Modal v-model="historyModal" footer-hide title="历史排名" width="800" :mask-closable="false">
+            <Card>
+                <div slot="title">关键词：椰子鞋 排名目标：第一页 第二名</div>
+                <div slot="extra">
+                    <DatePicker size="small" type="month" placeholder="年-月" style="width: 200px"></DatePicker>
+                </div>
+                <div class="content">
+                    <Table :columns="columns2" :data="data1"></Table>
+                    <div class="page">
+                        <Page></Page>
+                    </div>
+                </div>
+            </Card>
+
+        </Modal>
+    </div>
+
 </template>
 
 <script>
-  import User from '@/components/User.vue'
+    import User from '@/components/User.vue'
 
-  export default {
-    components: {
-      User
+    export default {
+        components: {
+            User
+        },
+        data() {
+            return {
+                historyModal: false,
+                columns1: [
+                    {
+                        align: 'center',
+                        title: '关键词',
+                        key: 'name'
+                    },
+                    {
+                        align: 'center',
+                        title: '排名目标',
+                        key: 'age'
+                    },
+                    {
+                        align: 'center',
+                        title: '排名',
+                        key: 'age'
+                    },
+                    {
+                        title: '宝贝标题',
+                        key: 'address'
+                    },
+                    {
+                        title: '查询时间',
+                        key: 'date'
+                    },
+                    {
+                        align: 'center',
+                        title: '操作',
+                        render: (h, params) => {
+                            return h('div', [
+                                h(
+                                    'Button',
+                                    {
+                                        style: 'margin:5px;color:#218FFF',
+                                        props: {size: 'small'},
+                                        on: {
+                                            click: () => {
+                                                this.historyModal = true
+                                            }
+                                        }
+                                    },
+                                    '历史排名'
+                                )
+                            ])
+                        }
+                    }
+                ],
+                columns2: [
+                    {
+                        align: 'center',
+                        title: '查询时间',
+                        key: 'date'
+                    },
+
+                    {
+                        align: 'center',
+                        title: '排名',
+                        key: 'age'
+                    },
+                    {
+                        title: '宝贝标题',
+                        key: 'address'
+                    }
+                ],
+                data1: [
+                    {
+                        name: '椰子鞋',
+                        age: '第 1 页 第 2 名',
+                        address: '淘皮猫INS超火的鞋子2019新…',
+                        date: '2019-01-02 11:20:22'
+                    },
+                    {
+                        name: '椰子鞋',
+                        age: '第 1 页 第 2 名',
+                        address: '淘皮猫INS超火的鞋子2019新…',
+                        date: '2019-01-02 11:20:22'
+                    },
+                    {
+                        name: '椰子鞋',
+                        age: '第 1 页 第 2 名',
+                        address: '淘皮猫INS超火的鞋子2019新…',
+                        date: '2019-01-02 11:20:22'
+                    },
+                    {
+                        name: '椰子鞋',
+                        age: '第 1 页 第 2 名',
+                        address: '淘皮猫INS超火的鞋子2019新…',
+                        date: '2019-01-02 11:20:22'
+                    }
+                ]
+            }
+        }
     }
-  }
 </script>
+<style scoped>
+    .search {
+        padding: 30px 20px 20px 20px;
+        background: #fff;
+        border-radius: 3px;
+        /* box-shadow: 0 2px 4px 0 rgba(222, 223, 224, 0.5); */
+    }
+
+    .search-options {
+        margin-right: 30px;
+        margin-bottom: 10px;
+        max-width: 500px;
+        display: inline-block;
+    }
+
+    .search-options-input {
+        width: 200px;
+        display: inline-block;
+    }
+
+    .search-sub {
+        margin-bottom: 10px;
+        display: inline-block;
+    }
+
+    .content {
+        padding: 10px 10px;
+        background: #fff;
+        border-radius: 4px;
+    }
+
+    .setting {
+        font-family: PingFangSC-Medium;
+        margin: 5px 0 15px 5px;
+    }
+
+    .blue {
+        font-size: 14px;
+        color: #218fff;
+        text-align: center;
+        line-height: 20px;
+    }
+
+    .btn {
+        margin-left: 25px;
+        width: 88px;
+    }
+
+    .page {
+        margin-top: 30px;
+        text-align: right;
+    }
+</style>
